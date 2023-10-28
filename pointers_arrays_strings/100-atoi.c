ff16854c35097b5168633b5373bf04b2f1f0093e
ali jezzini <ali.a.jezzini@gmail.com>
2023-10-28 13:45:51 -0700

    comm

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,41 +1,24 @@
+#include "main.h"
 #include <stdio.h>
+#include <string.h>
+/**
+ * _atoi - check the code
+ * @s: pointer
+ * Return: Always 0.
+ */
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int sign = 1;
 
-int _atoi(char *s) {
-    int result = 0;
-    int sign = 1; // Initialize sign to positive
-
-    // Skip leading whitespace
-    while (*s == ' ' || *s == '\t' || *s == '\n')
-        s++;
-
-    // Check for leading '+' or '-'
-    if (*s == '-') {
-        sign = -1;
-        s++;
-    } else if (*s == '+') {
-        s++;
-    }
-
-    // Iterate through the string and accumulate the numeric value
-    while (*s >= '0' && *s <= '9') {
-        // Check for integer overflow
-        if (result > (2147483647 - (*s - '0')) / 10) {
-            if (sign == 1) {
-                return 2147483647;
-            } else {
-                return -2147483648;
-            }
-        }
-        result = result * 10 + (*s - '0');
-        s++;
-    }
-
-    return result * sign;
-}
-
-int main() {
-    char str[] = "   -12345";
-    int result = _atoi(str);
-    printf("Result: %d\n", result);
-    return 0;
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if ((*s >= '0') && (*s <= '9'))
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+		break;
+	}
+		while (*s++);
+		return (num * sign);
 }
