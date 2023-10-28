f2919174ae9ad95ba3a0c115125906055dec145f
ali jezzini <ali.a.jezzini@gmail.com>
2023-10-28 13:39:08 -0700

    commit

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,11 +1,32 @@
-#include<stdio.h>
-#include"main.h"
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
 /**
-*_atoi - function
-*@s: variable
-*Return: int
+* _atoi -  Entrypoint
+* Description: 'the program's description _atoi
+* @str : 1 param
+*  Return: Always 0 (Success)
 */
-int _atoi(char *s)
+
+int _atoi(char *str)
 {
-	return (0);
+	int result, puiss, i = 0, j = 0;
+
+	result = 0;
+	puiss = 1;
+	while (('-' == *(str + j)) || (*(str + j) == '+'))
+	{
+		if (*str == '-')
+			puiss = puiss * -1;
+		j++;
+	}
+	while ((str[i] >= '0') && (str[i] <= '9'))
+	{
+		result = (result * 10) + (str[i] - '0');
+		i++;
+		if (*(str + i) < '0' || *(str + i) > '9' || *(str + i) == '\0')
+		break;
+	}
+	return (result * puiss);
 }
