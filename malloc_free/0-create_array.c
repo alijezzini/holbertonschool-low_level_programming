b4ddbbd3a8245a58bccbcb0ff2efde1da0bac7d6
ali jezzini <ali.a.jezzini@gmail.com>
2023-11-10 08:08:11 -0800

    Test

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,30 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include"main.h"
 
-char* create_array(unsigned int size, char c) {
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *a;
 
-    char* charArray = (char*)malloc(size * sizeof(char));
-
-    if (charArray == NULL) {
-        printf("Memory allocation failed.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    for (int i = 0; i < size; i++) {
-        charArray[i] = c;
-    }
-
-    return charArray;
-}
-
-int main(void) {
-    
-    int size = 10;
-
-    char initialValue = 'A';
-
-    char* myArray = create_array(size, initialValue);
-
-    return 0;
+	a = malloc((size) * sizeof(char));
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < size)
+	{
+		a[i] = c;
+		i++;
+	}
+	a[i] = '\0';
+	return (a);
 }
 
